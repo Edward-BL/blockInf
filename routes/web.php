dcdbc1375c5319a1b3782db22ea274199dd3b8d9
Edward-BL <rex.2002.lalo@gmail.com>
2023-10-12 13:07:36 -0600

    cambios de config

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('principal');
 });
+
+
+// "routes": [{
+//     "src": "/(.*)",
+//     "dest": "/api/index.php"
+// }],
